f7102286f337c79a126a5ce788d62be8e8e300bc
Adil Jamal <jamal.adil7860@gmail.com>
2026-04-15 00:40:57 +0530

    method

---
+++ src/Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    public static void main(String[] args) {
+        int ans = sum3(20,30);
+        System.out.println(ans);
+    }
+    // pass the value of number when you calling the method
+    static int sum3(int a,int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
