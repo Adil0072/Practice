13a3a3997eac37b80a5a9366fbd21bd6af2c7523
Adil Jamal <jamal.adil7860@gmail.com>
2026-04-15 00:17:18 +0530

    condition

---
+++ src/Ifelse.java
@@ -0,0 +1,13 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        int salary = 9000;
+        if(salary > 10000) {
+            salary += 2000;
+        }else if(salary > 20000) {
+            salary += 3000;
+        }else {
+            salary += 1000;
+        }
+        System.out.println(salary);
+    }
+}
