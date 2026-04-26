7725e6b77b53ae9cad98f341588368a480adf84f
Adil Jamal <jamal.adil7860@gmail.com>
2026-04-26 13:43:42 +0530

    return string

---
+++ src/ReturnString.java
@@ -0,0 +1,12 @@
+public class ReturnString {
+    public static void main(String[] args) {
+        String msg = greet();
+        System.out.println(msg);
+    }
+    static String greet(){
+//        String greeting = "hi";
+//        return greeting;
+        // or
+        return "hi";
+    }
+}
