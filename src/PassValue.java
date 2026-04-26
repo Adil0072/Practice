6c2d989dd8f47a1985748d2b1f1036f944ffdb33
Adil Jamal <jamal.adil7860@gmail.com>
2026-04-26 13:58:26 +0530

    passing string

--- src/PassValue.java
+++ src/PassValue.java
@@ -1,7 +1,13 @@
 public class PassValue {
     public static void main(String[] args) {
-        int ans = sum(10,10);
-        System.out.println(ans);
+//        int ans = sum(10,10);
+//        System.out.println(ans);
+        String new = mygreet("Adil");
+        System.out.println(new);
+    }
+    static String mygreet(String name){
+        String msg = "Hello " + name;
+        return msg;
     }
     static int sum(int a,int b){
         int sum = a+b;
